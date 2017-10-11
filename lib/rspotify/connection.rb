4fe124e86d867a83bddcd6d66a22ccf92272a507
Guillermo Kuster <hiperguille10@gmail.com>
2017-10-11 15:57:50 -0300

    add specific method to remove the bug

--- lib/rspotify/connection.rb
+++ lib/rspotify/connection.rb
@@ -62,6 +62,28 @@ module RSpotify
 
       headers = get_headers(params)
       headers['Accept-Language'] = ENV['ACCEPT_LANGUAGE'] if ENV['ACCEPT_LANGUAGE']
+
+      begin
+        response = RestClient.send(verb, url, *params)
+      rescue RestClient::Unauthorized
+        if @client_token
+          authenticate(@client_id, @client_secret)
+
+          obj = params.find{|x| x.is_a?(Hash) && x['Authorization']}
+          obj['Authorization'] = "Bearer #{@client_token}"
+
+          response = retry_connection verb, url, params
+        end
+      end
+    end
+
+    def send_request_with_user_credentials(verb, path, *params)
+      url = path.start_with?('http') ? path : API_URI + path
+
+      url, query = *url.split('?')
+      url = Addressable::URI.encode(url)
+      url << "?#{query}" if query
+
       response = RestClient.send(verb, url, *params)
 
       return response if raw_response
